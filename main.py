a37c9379169f640f4056dd6849e9c5a63df62615
Manthan M1 <mnthn.ai@gmail.com>
2023-08-31 21:09:36 -0700

    clean up & comment

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+"""
+A simple script to use the client to talk to the server
+"""
+
 from src.client import Client
 
+# Initialize the client
 client = Client()
+
+# Give the image path and the size of the image here
 image_path = "images/test_img.png"
+ouput_size = (200, 200)
+
+# Send the image to the server
 try:
-    client.post_image(image_path, (200,200))
+    client.post_image(image_path, ouput_size)
 except Exception as e:
+    # If the server is not running or in case of any other exception, this will be printed
     print("Failed to connect to server")
